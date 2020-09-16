318fbd5e8e0a0d94a2efdc1ac6d753d115740ab8
Rajed <luan.melo.faria@gmail.com>
2020-09-15 22:11:43 -0300

    agora ta valendo galerinha ehehe

--- app/src/main/java/com/example/wifiou4g_versao_1_0/MainActivity.java
+++ app/src/main/java/com/example/wifiou4g_versao_1_0/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        String oi;
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
