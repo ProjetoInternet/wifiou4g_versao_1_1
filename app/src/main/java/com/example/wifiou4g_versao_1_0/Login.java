b5e4fed340d720101ba52f48f7bdc937760398ec
Mickael <mickael.fronza@gmail.com>
2020-09-17 18:01:20 -0300

    interfaces

---
+++ app/src/main/java/com/example/wifiou4g_versao_1_0/Login.java
@@ -0,0 +1,14 @@
+package com.example.wifiou4g_versao_1_0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
