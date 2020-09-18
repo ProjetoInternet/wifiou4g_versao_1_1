c6d01954e463cfa47042ef1fb428cf279c9289db
Rajed <luan.melo.faria@gmail.com>
2020-09-18 00:11:47 -0300

    Ordenando telas

--- app/src/main/java/com/example/wifiou4g_versao_1_0/SignUp.java
+++ app/src/main/java/com/example/wifiou4g_versao_1_0/SignUp.java
@@ -1,8 +1,16 @@
 package com.example.wifiou4g_versao_1_0;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class SignUp extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+        TextView forgotpassword = findViewById(R.id.forgotpassword);
+        forgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              
+            }
+        });
+
+
     }
+
+    //by rajed
+    private void CallRegister(){
+
+
+    }
+       //by rajed.
+    private void CallForgotPassword(){
+
+
+    }
+
 }
